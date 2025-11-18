279ee5b5ec385621258c53957aaa592e08454e27
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-11-18 18:32:56 -0300

    feat: add route files and server conection

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import { router } from "./route.ts";
+
+const server = express();
+const port = 3000;
+
+server.use(express.json());
+server.use(router);
+
+server.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
