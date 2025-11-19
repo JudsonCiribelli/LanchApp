8b793a2630f40949e669b23af5605f86e8eb432f
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-11-19 14:46:57 -0300

    feat: add middleware to get user authenticated

---
+++ src/middleware/user/IsAutheticated.ts
@@ -0,0 +1,33 @@
+import type { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+interface IsAuthenticatedProps {
+  sub: string;
+}
+
+export function IsAuthenticated(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const authToken = req.headers.authorization;
+
+  if (!authToken) {
+    res.status(401).send({ message: "Auth token is required" }).end();
+  }
+
+  const [, token] = authToken!.split(" ");
+
+  try {
+    const { sub } = jwt.verify(
+      token as string,
+      process.env.JWT_SECRET!
+    ) as IsAuthenticatedProps;
+
+    req.userId = sub;
+    return next();
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({ message: "Authorization error!" }).end();
+  }
+}
