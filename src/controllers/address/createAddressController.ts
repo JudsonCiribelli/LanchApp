ca7fa1b748ccb8af1914c5dcd91a48ef90488208
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-11-20 17:05:09 -0300

    refactor: rename adress controller and service files

---
+++ src/controllers/address/createAddressController.ts
@@ -0,0 +1,32 @@
+import type { Request, Response } from "express";
+import { CreateAddressesService } from "../../services/address/createAddressService.ts";
+
+class CreateAddressesController {
+  async handle(req: Request, res: Response) {
+    const { street, number, complement, neighborhood, city, state, zipCode } =
+      req.body;
+
+    const userId = req.userId;
+
+    try {
+      const createAddressesService = new CreateAddressesService();
+
+      const addresses = await createAddressesService.execute({
+        userId,
+        street,
+        number,
+        complement,
+        neighborhood,
+        city,
+        state,
+        zipCode,
+      });
+
+      return res.status(201).send({ addresses });
+    } catch (error) {
+      console.log(error);
+      return res.status(400).send(error);
+    }
+  }
+}
+export { CreateAddressesController };
---
+++ src/controllers/address/getAddressController.ts
@@ -0,0 +1,21 @@
+import type { Request, Response } from "express";
+import { GetAddressesService } from "../../services/address/getAddressService.ts";
+
+class GetAddressesController {
+  async handle(req: Request, res: Response) {
+    const userId = req.userId;
+
+    try {
+      const getAddressesService = new GetAddressesService();
+
+      const userAddresses = await getAddressesService.execute({ userId });
+
+      return res.status(200).send({ userAddresses });
+    } catch (error) {
+      console.log(error);
+      return res.status(400).send(error);
+    }
+  }
+}
+
+export { GetAddressesController };
