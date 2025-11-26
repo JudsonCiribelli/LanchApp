a4740045b0bc4135bba053bf2e22fce93e6cbd8f
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-11-26 12:08:20 -0300

    feat: add zod validation to login user controller

--- src/controllers/user/LoginUserController.ts
+++ src/controllers/user/LoginUserController.ts
@@ -1,17 +1,31 @@
 import type { Request, Response } from "express";
 import { LoginUserService } from "../../services/user/loginUserService.ts";
+import z from "zod";
 
 class LoginUserController {
   async handle(req: Request, res: Response) {
-    const { email, password } = req.body;
+    const loginUserSchema = z.object({
+      email: z.email().nonempty({ message: "Email is required!" }),
+      password: z
+        .string()
+        .min(8, { message: "The password must be at least 8 characters long" }),
+    });
+
+    const { email, password } = loginUserSchema.parse(req.body);
 
     try {
       const loginUserService = new LoginUserService();
+
       const login = await loginUserService.execute({ email, password });
 
       return res.status(200).send({ login });
     } catch (error) {
-      console.log(error);
+      if (error instanceof z.ZodError) {
+        return res.status(400).json({
+          error: "Validation error",
+          issues: error.format(),
+        });
+      }
 
       return res.status(400).send(error);
     }
