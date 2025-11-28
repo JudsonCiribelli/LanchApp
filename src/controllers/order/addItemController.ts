59cbbeacd0615fa2ca3aaf1190506d0f54d3a6df
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-11-28 20:29:31 -0300

    feat: implement zod validation and improve error handling on add item controller

--- src/controllers/order/addItemController.ts
+++ src/controllers/order/addItemController.ts
@@ -1,11 +1,21 @@
 import type { Request, Response } from "express";
 import { AddItemService } from "../../services/order/addItemService.ts";
+import z from "zod";
 
 class AddItemController {
   async handle(req: Request, res: Response) {
-    const { orderId, productId, unitPrice, amount } = req.body;
+    const addItemSchema = z.object({
+      orderId: z.uuid().nonempty({ message: "Order Id is required!" }),
+      productId: z.uuid().nonempty({ message: "Product Id is required!" }),
+      unitPrice: z.string().nonempty({ message: "Unit price is required!" }),
+      amount: z.number().min(1),
+    });
 
     try {
+      const { orderId, productId, unitPrice, amount } = addItemSchema.parse(
+        req.body
+      );
+
       const addItemService = new AddItemService();
 
       const item = await addItemService.execute({
@@ -17,7 +27,13 @@ class AddItemController {
 
       return res.status(201).send({ item });
     } catch (error) {
-      console.log(error);
+      if (error instanceof z.ZodError) {
+        return res.status(400).json({
+          error: "Validation error",
+          issues: error.format(),
+        });
+      }
+
       return res.status(400).send(error);
     }
   }
