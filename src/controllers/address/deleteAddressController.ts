0da53cb18ce8bbedd795da200c86915b19dc8b8d
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-11-28 20:29:00 -0300

    feat: implement zod validation and improve error handling on delete address controller

--- src/controllers/address/deleteAddressController.ts
+++ src/controllers/address/deleteAddressController.ts
@@ -1,10 +1,16 @@
 import type { Request, Response } from "express";
 import { DeleteAdressService } from "../../services/address/deleteAddressService.ts";
+import z from "zod";
 
 class DeleteAdressController {
   async handle(req: Request, res: Response) {
+    const deleteAdressSchema = z.object({
+      addressId: z.uuid().nonempty({ message: "Adress id is required!" }),
+    });
+
     const userId = req.userId;
-    const { addressId } = req.body;
+
+    const { addressId } = deleteAdressSchema.parse(req.body);
 
     try {
       const deleteAdressService = new DeleteAdressService();
@@ -16,7 +22,13 @@ class DeleteAdressController {
 
       return res.status(200).send({ addressToDelete });
     } catch (error) {
-      console.log(error);
+      if (error instanceof z.ZodError) {
+        return res.status(400).json({
+          error: "Validation error",
+          issues: error.format(),
+        });
+      }
+
       return res.status(400).send(error);
     }
   }
