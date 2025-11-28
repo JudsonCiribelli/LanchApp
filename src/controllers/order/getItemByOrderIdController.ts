c5ce42493053bfa58960e0ea31f856876c63fbb1
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-11-28 20:30:06 -0300

    feat: implement zod validation and improve error handling on get item by order id controller

--- src/controllers/order/getItemByOrderIdController.ts
+++ src/controllers/order/getItemByOrderIdController.ts
@@ -1,18 +1,28 @@
 import type { Request, Response } from "express";
 import { GetItemsByOrderIdService } from "../../services/order/getItemByOrderIdService.ts";
+import z from "zod";
 
 class GetItemsByOrderIdController {
   async handle(req: Request, res: Response) {
-    const { orderId } = req.body;
+    const getItemsByOrderIdSchema = z.object({
+      orderId: z.uuid().nonempty({ message: "Order id is required!" }),
+    });
 
     try {
+      const { orderId } = getItemsByOrderIdSchema.parse(req.body);
+
       const getItems = new GetItemsByOrderIdService();
 
       const items = await getItems.execute({ orderId });
 
       return res.status(200).send({ items });
     } catch (error) {
-      console.log(error);
+      if (error instanceof z.ZodError) {
+        return res.status(400).json({
+          error: "Validation error",
+          issues: error.format(),
+        });
+      }
 
       return res.status(400).send(error);
     }
