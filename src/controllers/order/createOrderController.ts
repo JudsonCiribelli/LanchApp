8386f1c219597bf1a3fb3f0e46f5bb783cb6453b
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-11-28 20:29:44 -0300

    feat: implement zod validation and improve error handling on  create order controller

--- src/controllers/order/createOrderController.ts
+++ src/controllers/order/createOrderController.ts
@@ -1,12 +1,29 @@
 import type { Request, Response } from "express";
 import { CreateOrderService } from "../../services/order/createOrderService.ts";
+import z from "zod";
+import { OrderType } from "@prisma/client";
 
 class CreateOrderController {
   async handle(req: Request, res: Response) {
-    const { table, name, addressId, type } = req.body;
     const userId = req.userId;
 
+    const createOrderSchema = z.object({
+      table: z.number().min(1),
+      name: z.string().nonempty({ message: "Your name is required!" }),
+      addressId: z.uuid(),
+      type: z
+        .enum(OrderType)
+        .refine((val) => Object.values(OrderType).includes(val), {
+          message:
+            "Invalid or missing status. Accepted values: PENDING, IN_PREPARATION, READY, ON_THE_WAY, DELIVERED, CANCELED, FINISHED",
+        }),
+    });
+
     try {
+      const { table, name, addressId, type } = createOrderSchema.parse(
+        req.body
+      );
+
       const createOrderService = new CreateOrderService();
 
       const order = await createOrderService.execute({
@@ -19,7 +36,13 @@ class CreateOrderController {
 
       return res.status(201).send({ order });
     } catch (error) {
-      console.log(error);
+      if (error instanceof z.ZodError) {
+        return res.status(400).json({
+          error: "Validation error",
+          issues: error.format(),
+        });
+      }
+
       return res.status(400).send(error);
     }
   }
