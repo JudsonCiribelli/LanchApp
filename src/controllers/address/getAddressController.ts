810f9cf8f8cdf7fc2a9d2e48ba5c5bfa00a3c615
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-11-28 20:29:18 -0300

    feat: improve error handling on get address controller

--- src/controllers/address/getAddressController.ts
+++ src/controllers/address/getAddressController.ts
@@ -1,5 +1,6 @@
 import type { Request, Response } from "express";
 import { GetAddressService } from "../../services/address/getAddressService.ts";
+import z from "zod";
 
 class GetAddressController {
   async handle(req: Request, res: Response) {
@@ -12,7 +13,13 @@ class GetAddressController {
 
       return res.status(200).send({ userAddresses });
     } catch (error) {
-      console.log(error);
+      if (error instanceof z.ZodError) {
+        return res.status(400).json({
+          error: "Validation error",
+          issues: error.format(),
+        });
+      }
+
       return res.status(400).send(error);
     }
   }
