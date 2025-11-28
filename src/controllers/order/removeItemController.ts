c1678ca56d77dda0e7e28a598bed455f4ac0ed30
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-11-28 20:31:06 -0300

    feat:  improve error handling on remove item controller

--- src/controllers/order/removeItemController.ts
+++ src/controllers/order/removeItemController.ts
@@ -1,17 +1,24 @@
 import type { Request, Response } from "express";
 import { RemoveItemService } from "../../services/order/removeItemService.ts";
+import z from "zod";
 
 class RemoveItemController {
   async handle(req: Request, res: Response) {
-    const itemId = req.query.itemId as string;
     try {
+      const itemId = req.query.itemId as string;
+
       const removeItemService = new RemoveItemService();
 
       const itemToDelete = await removeItemService.execute({ itemId });
 
       return res.status(200).send({ itemToDelete });
     } catch (error) {
-      console.log(error);
+      if (error instanceof z.ZodError) {
+        return res.status(400).json({
+          error: "Validation error",
+          issues: error.format(),
+        });
+      }
 
       return res.status(400).send(error);
     }
