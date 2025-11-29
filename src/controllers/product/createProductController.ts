273b4afa6c9617ece33589dcf4aa476a85760b06
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-11-29 11:28:09 -0300

    feat: implement zod validation and improve error handling on create product controller

--- src/controllers/product/createProductController.ts
+++ src/controllers/product/createProductController.ts
@@ -1,12 +1,22 @@
 import type { Request, Response } from "express";
 import { CreateProductService } from "../../services/product/createProductService.ts";
+import z from "zod";
 
 class CreateProductController {
   async handle(req: Request, res: Response) {
-    const { categoryId, name, price, description } = req.body;
     const userId = req.userId;
 
+    const createProductSchema = z.object({
+      categoryId: z.uuid().nonempty({ message: "Category ID is required!" }),
+      name: z.string().nonempty({ message: "Name is required!" }),
+      price: z.string().nonempty({ message: "Price is required!" }),
+      description: z.string().nonempty({ message: "Description is required" }),
+    });
+
     try {
+      const { categoryId, name, price, description } =
+        createProductSchema.parse(req.body);
+
       const createProductService = new CreateProductService();
 
       if (!req.file) {
@@ -25,7 +35,12 @@ class CreateProductController {
         return res.status(201).send({ product });
       }
     } catch (error) {
-      console.log(error);
+      if (error instanceof z.ZodError) {
+        return res.status(400).json({
+          error: "Validation error",
+          issues: error.format(),
+        });
+      }
 
       return res.status(400).send(error);
     }
