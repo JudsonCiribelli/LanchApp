80152a78b398856abad7d316440038852d32ea6d
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-11-29 11:28:59 -0300

    feat: improve error handling on get product controller

--- src/controllers/product/getProductController.ts
+++ src/controllers/product/getProductController.ts
@@ -1,5 +1,6 @@
 import type { Request, Response } from "express";
 import { GetProductService } from "../../services/product/getProductService.ts";
+import z from "zod";
 
 class GetProductController {
   async handle(req: Request, res: Response) {
@@ -7,9 +8,16 @@ class GetProductController {
       const getProductService = new GetProductService();
 
       const products = await getProductService.execute();
+
       return res.status(200).send({ products });
     } catch (error) {
-      console.log(error);
+      if (error instanceof z.ZodError) {
+        return res.status(400).json({
+          error: "Validation error",
+          issues: error.format(),
+        });
+      }
+
       return res.status(400).send(error);
     }
   }
