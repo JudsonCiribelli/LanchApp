5f98d53a4d8b34483e09c42e087e76cf89f9d4f9
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-11-29 11:27:39 -0300

    feat: implement zod validation and improve error handling on send order controller

--- src/controllers/order/sendOrderController.ts
+++ src/controllers/order/sendOrderController.ts
@@ -1,17 +1,28 @@
 import type { Request, Response } from "express";
 import { SendOrderService } from "../../services/order/sendOrderService.ts";
+import z from "zod";
 class SendOrderController {
   async handle(req: Request, res: Response) {
-    const { orderId } = req.body;
+    const sendOrderSchema = z.object({
+      orderId: z.uuid().nonempty({ message: "Order Id is required!" }),
+    });
 
     try {
+      const { orderId } = sendOrderSchema.parse(req.body);
+
       const sendOrderService = new SendOrderService();
 
       const order = await sendOrderService.execute({ orderId });
 
       return res.status(200).send({ order });
     } catch (error) {
-      console.log(error);
+      if (error instanceof z.ZodError) {
+        return res.status(400).json({
+          error: "Validation error",
+          issues: error.format(),
+        });
+      }
+
       return res.status(400).send(error);
     }
   }
