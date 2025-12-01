6b438073008c1349a05b13f9c90d517a57818579
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-12-01 11:05:56 -0300

    feat: add factorie to create a new user

---
+++ src/tests/factories/user/makeUser.ts
@@ -0,0 +1,19 @@
+import { hash } from "bcryptjs";
+import { randomUUID } from "crypto";
+import { faker } from "@faker-js/faker";
+import prismaClient from "../../../lib/client.ts";
+
+export const makeUser = async () => {
+  const passwordHash = randomUUID().slice(0, 8);
+
+  const user = await prismaClient.user.create({
+    data: {
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      password: await hash(passwordHash, 8),
+      phone: faker.phone.number(),
+    },
+  });
+
+  return user;
+};
