9176fb59331e8dc45e613b86994c671bb315ab7f
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-12-02 11:11:06 -0300

    feat: add get user orders test

---
+++ src/tests/user/getUserOrders.test.ts
@@ -0,0 +1,29 @@
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import prismaClient from "../../lib/client.ts";
+import supertest from "supertest";
+import { server } from "../../server.ts";
+import { makeAuthenticatedUser } from "../factories/user/makeAuthenticatedUser.ts";
+
+describe("/user/orders", () => {
+  beforeAll(async () => {});
+
+  afterAll(async () => {
+    await prismaClient.user.deleteMany();
+    await prismaClient.$disconnect();
+  });
+
+  it("should return user orders", async () => {
+    const { token } = await makeAuthenticatedUser();
+
+    const response = await supertest(server)
+      .get("/user/orders")
+      .set("Content-Type", "application/json")
+      .set("Authorization", `Bearer ${token}`);
+
+    expect(response.body).toEqual({
+      userOrders: [],
+    });
+
+    expect(response.status).toBe(200);
+  });
+});
