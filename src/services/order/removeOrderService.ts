11304c87fb454e2f71deadd16d46b3ca2975860f
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-12-09 19:57:02 -0300

    refactor: enforce ownership check to prevent unauthorized deletion

--- src/services/order/removeOrderService.ts
+++ src/services/order/removeOrderService.ts
@@ -2,17 +2,43 @@ import prismaClient from "../../lib/client.ts";
 
 interface RemoveOrderProps {
   orderId: string;
+  userId: string;
 }
 
 class RemoveOrderService {
-  async execute({ orderId }: RemoveOrderProps) {
+  async execute({ orderId, userId }: RemoveOrderProps) {
+    const user = await prismaClient.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (user!.role !== "ADMIN") {
+      throw new Error("You do not have permission to delete this order.");
+    }
+
+    const order = await prismaClient.order.findFirst({
+      where: {
+        id: orderId,
+        userId: userId,
+      },
+    });
+
+    if (!order) {
+      throw new Error("Order not found or you do not have permission.");
+    }
+
+    if (order.status !== "PENDING" && order.status !== "FINISHED") {
+      throw new Error("Cannot delete a processed order");
+    }
+
     const orderToDelete = await prismaClient.order.delete({
       where: {
         id: orderId,
       },
     });
 
-    return { orderToDelete };
+    return orderToDelete;
   }
 }
 
