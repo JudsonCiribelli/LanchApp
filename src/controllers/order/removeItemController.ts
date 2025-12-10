bd633c7bdeb8e5f03210a87fcd4e6e24ccf39ac5
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-12-10 11:53:04 -0300

    fix: validate removal based on draft boolean field

--- src/controllers/order/removeItemController.ts
+++ src/controllers/order/removeItemController.ts
@@ -1,26 +1,27 @@
 import type { Request, Response } from "express";
 import { RemoveItemService } from "../../services/order/removeItemService.ts";
-import z from "zod";
 
 class RemoveItemController {
   async handle(req: Request, res: Response) {
+    const userId = req.userId;
     try {
       const itemId = req.query.itemId as string;
 
+      if (!itemId) {
+        return res.status(400).json({ error: "Item ID is required" });
+      }
+
       const removeItemService = new RemoveItemService();
 
-      const itemToDelete = await removeItemService.execute({ itemId });
+      const itemToDelete = await removeItemService.execute({ itemId, userId });
 
       return res.status(200).send({ itemToDelete });
     } catch (error) {
-      if (error instanceof z.ZodError) {
-        return res.status(400).json({
-          error: "Validation error",
-          issues: error.format(),
-        });
+      if (error instanceof Error) {
+        return res.status(400).json({ error: error.message });
       }
 
-      return res.status(400).send(error);
+      return res.status(500).json({ error: "Internal server error" });
     }
   }
 }
--- src/services/order/removeItemService.ts
+++ src/services/order/removeItemService.ts
@@ -2,17 +2,43 @@ import prismaClient from "../../lib/client.ts";
 
 interface RemoveItemProps {
   itemId: string;
+  userId: string;
 }
 
 class RemoveItemService {
-  async execute({ itemId }: RemoveItemProps) {
+  async execute({ itemId, userId }: RemoveItemProps) {
+    const item = await prismaClient.item.findUnique({
+      where: {
+        id: itemId,
+      },
+      include: {
+        order: true,
+      },
+    });
+
+    if (!item) {
+      throw new Error("Item not found.");
+    }
+
+    if (item.order.userId !== userId) {
+      throw new Error("You do not have permission to delete this item.");
+    }
+
+    const isDraft = item.order.draft;
+    const isPending = item.order.status === "PENDING";
+
+    if (!isDraft && !isPending) {
+      throw new Error(
+        "Cannot remove items from an order that is already being processed."
+      );
+    }
     const itemToDelete = await prismaClient.item.delete({
       where: {
         id: itemId,
       },
     });
 
-    return { itemToDelete };
+    return itemToDelete;
   }
 }
 
