35e05cf6ceebf1b20a4f290111408ac42cd766b4
Judson Ciribelli <judson.ciribelli17@gmail.com>
2026-02-02 15:19:31 -0300

    fix: ensure server will reading files

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,26 @@
 import express, { type Request, type Response } from "express";
 import { router } from "./route.ts";
 import swaggerUi from "swagger-ui-express";
-import swaggerDocument from './swagger.json' with { type: "json" };
+import swaggerDocument from "./swagger.json" with { type: "json" };
+import path from "path";
+import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const server = express();
 
 const port = 3030;
 
 server.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 server.use(express.json());
+
+server.use("/files", express.static(path.resolve(__dirname, "..", "tmp")));
+
 server.use(router);
 
-server.use((err: Error, req: Request, res: Response) => {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any
+server.use((err: Error, req: Request, res: Response, next: any) => {
   if (err instanceof Error) {
     return res.status(400).json({
       error: err.message,
