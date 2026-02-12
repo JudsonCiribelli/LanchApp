b2debed80c572567c712357530f867f9bf30c828
Judson Ciribelli <judson.ciribelli17@gmail.com>
2026-02-11 21:00:01 -0300

    feat: add erros validation on get order by id service and controller

--- src/controllers/order/getOrderByIdController.ts
+++ src/controllers/order/getOrderByIdController.ts
@@ -1,19 +1,36 @@
 import type { Request, Response } from "express";
 import { GetOrderByIdService } from "../../services/order/getOrderByIdService.ts";
+import z from "zod";
 
 class GetOrderByIdController {
   async handle(req: Request, res: Response) {
+    const userId = req.userId as string;
+
     try {
       const orderId = req.query.orderId as string;
 
+      if (!orderId) {
+        return res.status(400).json({ error: "Missing orderId" });
+      }
+
       const getOrderByIdService = new GetOrderByIdService();
 
-      const order = await getOrderByIdService.execute({ orderId });
+      const order = await getOrderByIdService.execute({ orderId, userId });
 
       return res.status(200).send(order);
     } catch (error) {
-      console.log(error);
-      return res.status(400).send(error);
+      if (error instanceof z.ZodError) {
+        return res.status(400).json({
+          error: "Validation error",
+          issues: error.format(),
+        });
+      }
+
+      if (error instanceof Error) {
+        return res.status(400).json({ error: error.message });
+      }
+
+      return res.status(500).json({ error: "Internal server error" });
     }
   }
 }
--- src/services/order/getOrderByIdService.ts
+++ src/services/order/getOrderByIdService.ts
@@ -2,10 +2,29 @@ import prismaClient from "../../lib/client.ts";
 
 interface GetOrderProps {
   orderId: string;
+  userId: string;
 }
 
 class GetOrderByIdService {
-  async execute({ orderId }: GetOrderProps) {
+  async execute({ userId, orderId }: GetOrderProps) {
+    const user = await prismaClient.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!user) {
+      throw new Error("User not found.");
+    }
+
+    if (user.role !== "ADMIN") {
+      throw new Error("You do not have permission to access this resource.");
+    }
+
+    if (!orderId) {
+      throw new Error("Order id is required.");
+    }
+
     const order = await prismaClient.order.findUnique({
       where: {
         id: orderId,
